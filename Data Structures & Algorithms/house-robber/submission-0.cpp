a914fdd40db033b724000d163735781e4b05bd17
Laeeq Ahmed <laeeq.amd@gmail.com>
2026-09-09 17:15:16 +0530

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int solve(int idx, vector<int> &nums, vector<int> &dp){
+        if(idx<0) return 0;
+        if(dp[idx] != -1) return dp[idx];
+        int take = nums[idx]+solve(idx-2, nums, dp);
+        int notTake = solve(idx-1, nums,dp);
+        return dp[idx] = max(take,notTake);
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n+1,-1);
+        return solve(n-1,nums, dp);
+    }
+};
